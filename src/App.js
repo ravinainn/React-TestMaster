eb140093ba0d2d7e3469e6261c0d938131c97a41
ravinainn <nain.ravi12345@gmail.com>
2023-08-05 15:30:30 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './Styles/style.css'
+import Form1 from './components/Form1';
+import Form2 from './components/Form2';
+import Form3 from './components/Form3';
+import Form4 from './components/Form4';
 
-function App() {
+const App = () => {
+  const [step, setStep] = useState(1);
+  const [formData, setFormData] = useState({
+    testName: '',
+    testDescription: '',
+    duration: '',
+    numOfQuestions: '',
+    testImage: null,
+    instruction: '',
+    selectedTest: '',
+    categoryName: '',
+    numOfCategoryQuestions: '',
+    questionImage: null,
+    questionText: '',
+    questionTime: '',
+    questionWeightage: '',
+    MCQ1: '',
+    MCQ2: '',
+    MCQ3: '',
+    MCQ4: '',
+    MCQ1Weightage: '',
+    MCQ2Weightage: '',
+    MCQ3Weightage: '',
+    MCQ4Weightage: '',
+
+
+
+    // Add other fields
+  });
+
+  const handleChange = (e) => {
+    const { name, value, type, files } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: type === 'file' ? files[0] : value,
+    }));
+  };
+
+  const handleBack = () => {
+    if (step > 1) {
+      setStep(step - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (step < 4) {
+      setStep(step + 1);
+    }
+  };
+
+  const handleSaveAndContinue = () => {
+    // Implement your logic here for saving form data
+    handleNext();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {step === 1 && <Form1 data={formData} onNext={handleNext} handleChange={handleChange} />}
+      {step === 2 && <Form2 data={formData} onBack={handleBack} onNext={handleNext} handleChange={handleChange} />}
+      {step === 3 && <Form3 data={formData} onBack={handleBack} onNext={handleNext} handleChange={handleChange} />}
+      {step === 4 && <Form4 data={formData} onBack={handleBack} onSaveContinue={handleSaveAndContinue} handleChange={handleChange} />}
     </div>
   );
-}
+};
 
 export default App;
+
---
+++ src/components/Form1.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+
+const Form1 = ({ data, onNext, handleChange }) => {
+    return (
+        <div className="main">
+            <div className="form-container">
+                <h2>Test Master</h2>
+                <div className="input-flex">
+                    <div className="input-flex-item">
+                        <label>Test Name:</label>
+                        <input
+                            type="text"
+                            name="testName"
+                            value={data.testName}
+                            onChange={handleChange}
+                            placeholder="Test Name"
+                            required
+                        />
+
+                    </div>
+                    <div className="input-flex-item">
+
+                        <label>Test Description:</label>
+                        <textarea
+                            name="testDescription"
+                            value={data.testDescription}
+                            onChange={handleChange}
+                            placeholder="Test Description"
+                            
+                        />
+                    </div>
+                    <div className="input-flex-item">
+
+                        <label>Duration of Test (minutes):</label>
+                        <input
+                            type="number"
+                            name="duration"
+                            value={data.duration}
+                            onChange={handleChange}
+                            placeholder="Duration of Test (minutes)"
+                            required
+                        />
+                    </div>
+                    <div className="input-flex-item">
+
+                        <label>Number of Questions:</label>
+                        <input
+                            type="number"
+                            name="numOfQuestions"
+                            value={data.numOfQuestions}
+                            onChange={handleChange}
+                            placeholder="Number of Questions"
+                        />
+                    </div>
+                    <div className="input-flex-item">
+
+                        <label>Image for Test:</label>
+                        <input
+                            type="file"
+                            name="testImage"
+                            onChange={handleChange}
+                        />
+                    </div>
+
+                </div>
+                <div className="btn-div">
+                    <button onClick={onNext}>Save & Continue</button>
+
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default Form1;
+
+
